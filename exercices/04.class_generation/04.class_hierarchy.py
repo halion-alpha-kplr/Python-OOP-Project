d044538c063ab3e9872c6e90bae2c78962d1f60c
halion-alpha-kplr <124892038+halion-alpha-kplr@users.noreply.github.com>
2023-03-08 15:47:51 +0000

    jean-benoit

--- exercices/04.class_generation/04.class_hierarchy.py
+++ exercices/04.class_generation/04.class_hierarchy.py
@@ -3,10 +3,21 @@ import json
 from unidecode import unidecode
 import re
 import os
+import class_generation_exo_perso
+
+def trimspaces(data):
+    import re
+    # Define a regular expression pattern to match quoted substrings
+    pattern = r'"[^"]*"'
+    # Replace spaces and hyphens with underscore
+    #return re.sub(pattern, lambda m: m.group(0).replace(" ", "_").replace("-", "_"), str(unidecode(json.dumps(data))))
+    data_s=json.dumps(data)
+    return re.sub(pattern, lambda m: m.group(0).replace(" ", "_").replace("-", "_"), data_s)
 
 # Charger des données JSON à partir du fichier dans un dictionnaire python
 local_path = os.path.dirname(os.path.abspath(__file__))
 json_data = json.load(open(os.path.join(local_path, 'json_data.json'), "rb"))
+json_data = trimspaces(json_data)
 
 # Reconvertir le dictionnaire en chaine de caractere pour le traiter ensuite
 json_str = json.dumps(json_data)
---
+++ exercices/04.class_generation/class_generation_exo_perso.py
@@ -0,0 +1,54 @@
+import json
+from unidecode import unidecode
+import os
+
+def json_dict_from_file():
+    local_path = os.path.dirname(os.path.abspath(__file__))
+    json_data = json.load(open(os.path.join(local_path, 'json_data.json'), "rb"))
+    json_str = json.dumps(json_data)
+    json_data = unidecode(json_str)
+    json_dict = json.loads(json_data)
+    return json_dict
+
+def generate_class_def(class_name: str, attrs:dict, superclass_name:str,superclass_args:list=[]):    
+    
+    constructor_args = []
+    constructor_def = ""
+    has_attributes = False
+
+    class_template = f"class {class_name}"
+
+    if superclass_name:        
+        class_template += f"({superclass_name})"
+    
+    class_template += ":\n"
+
+    for attr_name in attrs.keys():
+        if attr_name != "subclasses":
+            has_attributes = True
+            constructor_args.append(attr_name)
+            constructor_def += f"\n\t\tself.{attr_name} = {attr_name}"
+
+    if class_name == "Product":
+            constructor_def += "\n\t\tself.name=type(self).__name__"
+
+    if has_attributes:
+        constructor_template = f"\tdef __init__(self, {', '.join(constructor_args+superclass_args)}):"
+
+        if len(superclass_args)>0:
+            constructor_template +=f"\n\t\tsuper().__init__({', '.join(superclass_args)})"
+
+        constructor_template +=constructor_def
+    
+    else:
+        if len(superclass_args)>0:
+            constructor_template = f"\tdef __init__(self, {', '.join(superclass_args)}):"
+            constructor_template +=f"\n\t\tsuper().__init__({', '.join(superclass_args)})"
+      
+        else:    
+            constructor_template = "\tpass"
+
+    return class_template + constructor_template + "\n\n"
+
+test_solution = generate_class_def("test",json_dict_from_file(),"test",)
+print(test_solution)
